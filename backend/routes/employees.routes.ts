a23363f638eb32b176878c3b901717ac975b03fd
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-09-10 16:03:42 +0100

    - never mind the errors, just lazy to make working and typed controllers
    - itll get better, i promise<void>

--- backend/routes/employees.routes.ts
+++ backend/routes/employees.routes.ts
@@ -1,86 +1,20 @@
-import employees from "../model/employees.json";
-
-const data = {
-	employees: employees,
-	setEmployees: function (data: any) {
-		this.employees = data;
-	},
-};
-
-const getAllEmployees = (req, res) => {
-	res.json(data.employees);
-};
-
-const createNewEmployee = (req, res) => {
-	const newEmployee = {
-		id: data.employees[data.employees.length - 1].id + 1 || 1,
-		firstname: req.body.firstname,
-		lastname: req.body.lastname,
-	};
-
-	if (!newEmployee.firstname || !newEmployee.lastname) {
-		return res
-			.status(400)
-			.json({ message: "First and last names are required" });
-	}
-
-	data.setEmployees([...data.employees, newEmployee]);
-	res.status(201).json(data.employees);
-};
-
-const updateEmployee = (req, res) => {
-	const employee = data.employees.find(
-		(emp) => emp.id === parseInt(req.body.id)
-	);
-	if (!employee) {
-		return res
-			.status(400)
-			.json({ message: `Employee ID ${req.body.id} not found` });
-	}
-	if (req.body.firstname) employee.firstname = req.body.firstname;
-	if (req.body.lastname) employee.lastname = req.body.lastname;
-	const filteredArray = data.employees.filter(
-		(emp) => emp.id !== parseInt(req.body.id)
-	);
-	const unsortedArray = [...filteredArray, employee];
-	data.setEmployees(
-		unsortedArray.sort((a, b) => (a.id > b.id ? 1 : a.id < b.id ? -1 : 0))
-	);
-	res.json(data.employees);
-};
-
-const deleteEmployee = (req, res) => {
-	const employee = data.employees.find(
-		(emp) => emp.id === parseInt(req.body.id)
-	);
-	if (!employee) {
-		return res
-			.status(400)
-			.json({ message: `Employee ID ${req.body.id} not found` });
-	}
-	const filteredArray = data.employees.filter(
-		(emp) => emp.id !== parseInt(req.body.id)
-	);
-	data.setEmployees([...filteredArray]);
-	res.json(data.employees);
-};
-
-const getEmployee = (req, res) => {
-	const employee = data.employees.find(
-		(emp) => emp.id === parseInt(req.body.id)
-	);
-	if (!employee) {
-		return res
-			.status(400)
-			.json({ message: `Employee ID ${req.body.id} not found` });
-	}
-	res.json(employee);
-};
-
-module.exports = {
+import express from "express";
+import {
+	createNewEmployee,
+	deleteEmployee,
 	getAllEmployees,
 	getEmployee,
-	createNewEmployee,
 	updateEmployee,
-	deleteEmployee,
-};
+} from "../controllers/employees.controller";
+const router = express.Router();
+
+router
+	.route("/")
+	.get(getAllEmployees)
+	.post(createNewEmployee)
+	.put(updateEmployee)
+	.delete(deleteEmployee);
+
+router.route("/:id").get(getEmployee);
+
+module.exports = router;
