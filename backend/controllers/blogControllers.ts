3fa2796e4dd2ad074dabd454fc8949bc3a9aa4f1
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-09-30 22:33:15 +0100

    - created skeleton blog route controllers

--- backend/controllers/blogControllers.ts
+++ backend/controllers/blogControllers.ts
@@ -1,10 +1,56 @@
 import { Request, Response } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import mongoose from "mongoose";
-import blogModel from "../models/blog.model";
+import Blogs from "../models/blog.model";
 import { BlogPostType } from "../types";
 import { createResponse } from "../utils";
 
-export const allBlogPosts = (req: Request, res: Response) => {
-	res.send("this is the blog route");
+export const allBlogPosts = async (req: Request, res: Response) => {
+	try {
+		const blogs = await Blogs.find().lean();
+		res.json(
+			createResponse({
+				_code: 200,
+				_meaning: "Fetched all blog posts",
+				data: blogs,
+			})
+		);
+	} catch (error) {
+		console.error(error);
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+			createResponse({
+				_code: StatusCodes.INTERNAL_SERVER_ERROR,
+				_meaning: ReasonPhrases.INTERNAL_SERVER_ERROR,
+				message: `${(error as Error).message}`,
+			})
+		);
+	}
 };
+
+export const getBlogPost = async (req: Request, res: Response) => {
+	const { id } = req.body;
+	try {
+		const blogPost = await Blogs.findById(id).lean();
+		res.json(
+			createResponse({
+				_code: 200,
+				_meaning: "Fetched blog post",
+				data: [blogPost],
+			})
+		);
+	} catch (error) {
+		console.error(error);
+		return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(
+			createResponse({
+				_code: StatusCodes.INTERNAL_SERVER_ERROR,
+				_meaning: ReasonPhrases.INTERNAL_SERVER_ERROR,
+				message: `${(error as Error).message}`,
+			})
+		);
+	}
+};
+
+export const createBlogPost = async (req: Request, res: Response) => {};
+
+export const editBlogPost = async (req: Request, res: Response) => {};
+
+export const deleteBlogPost = async (req: Request, res: Response) => {};
--- backend/routes/blog.routes.ts
+++ backend/routes/blog.routes.ts
@@ -1,8 +1,23 @@
 import express from "express";
 import { protectedAction } from "../middleware/auth/protectedActions";
-import { allBlogPosts } from "../controllers/blogControllers";
+import {
+	allBlogPosts,
+	createBlogPost,
+	deleteBlogPost,
+	editBlogPost,
+	getBlogPost,
+} from "../controllers/blogControllers";
 const router = express.Router();
 
 router.get("/", allBlogPosts);
+
+router.post("/", protectedAction, createBlogPost);
+
+router.get(":id", getBlogPost);
+
+router.put(":id", protectedAction, editBlogPost);
+
+router.delete(":id", protectedAction, deleteBlogPost);
+
 
 export default router;
