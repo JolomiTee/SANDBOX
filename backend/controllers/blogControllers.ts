1927afefa1828ba7e6b5164355d534d9a860942f
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-01 02:34:26 +0100

    - successfully deleted blogs

--- backend/controllers/blogControllers.ts
+++ backend/controllers/blogControllers.ts
@@ -120,4 +120,21 @@ export const createBlogPost = async (req: ExtReq, res: Response) => {
 
 export const editBlogPost = async (req: Request, res: Response) => {};
 
-export const deleteBlogPost = async (req: Request, res: Response) => {};
+export const deleteBlogPost = async (req: ExtReq, res: Response) => {
+	try {
+		const blog = await Blogs.findById(req.params.id);
+		if (!blog) {
+			return res.status(404).json({ error: "Blog not found" });
+		}
+		if (blog.author.toString() !== req.user?._id.toString()) {
+			return res.status(401).json({ error: "You cannot delete this blog" });
+		}
+
+		await Blogs.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Blog deleted sucesfully" });
+	} catch (error) {
+		console.log("Error in deletePost controller", (error as Error).message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
